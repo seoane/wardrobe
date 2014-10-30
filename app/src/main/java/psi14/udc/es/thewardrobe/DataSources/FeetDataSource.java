ae5f00a5a907f6764e91ae750ac05c817747626e
berto <csberto@gmail.com>
2014-10-30 13:53:21 +0100

    Added FeetDataSource and LegsDataSource

---
+++ app/src/main/java/psi14/udc/es/thewardrobe/DataSources/FeetDataSource.java
@@ -0,0 +1,13 @@
+package psi14.udc.es.thewardrobe.DataSources;
+
+import android.content.Context;
+
+/**
+ * Created by berto on 30/10/14.
+ */
+public class FeetDataSource extends ClothDataSource{
+
+    public FeetDataSource(Context context) {
+        super(context);
+    }
+}
---
+++ app/src/main/java/psi14/udc/es/thewardrobe/DataSources/LegsDataSource.java
@@ -0,0 +1,12 @@
+package psi14.udc.es.thewardrobe.DataSources;
+
+import android.content.Context;
+
+/**
+ * Created by berto on 30/10/14.
+ */
+public class LegsDataSource extends ClothDataSource {
+    public LegsDataSource(Context context) {
+        super(context);
+    }
+}
