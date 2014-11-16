41e6cca1c99c3609f8d1d897dfa430d192996c44
Jesus <suso@outlook.com>
2014-11-16 21:17:52 +0100

    Fragment Uploaded

--- app/src/main/java/psi14/udc/es/thewardrobe/CombinatorFragment.java
+++ app/src/main/java/psi14/udc/es/thewardrobe/CombinatorFragment.java
@@ -1,30 +1,85 @@
 package psi14.udc.es.thewardrobe;
 
-
-import android.app.Fragment;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import psi14.udc.es.thewardrobe.ControlLayer.Cloth;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class CombinatorFragment extends Fragment {
+import static psi14.udc.es.thewardrobe.Utils.Constants.APP_TAG;
+import static psi14.udc.es.thewardrobe.Utils.Constants.DEBUG;
+import static psi14.udc.es.thewardrobe.Utils.Constants.ID;
+import static psi14.udc.es.thewardrobe.Utils.Constants.PARCELABLE_CLOTH_KEY;
+import static psi14.udc.es.thewardrobe.Utils.Utilities.cancelPotentialWork;
 
+public class CombinatorFragment extends Fragment implements ImageView.OnClickListener {
+    public Resources res;
+    public String activityTag = "CombinerFragment";
+    public Cloth cloth;
+    public ImageView imageView;
+    Bitmap mPlaceHolderBitmap;
 
     public CombinatorFragment() {
-        // Required empty public constructor
     }
 
+    public static CombinatorFragment newInstance(Cloth cloth) {
+        CombinatorFragment combinerFragment = new CombinatorFragment();
+        Bundle frag1Args = new Bundle();
+        frag1Args.putParcelable(PARCELABLE_CLOTH_KEY, cloth);
+        combinerFragment.setArguments(frag1Args);
+        combinerFragment.setRetainInstance(true);
+        return combinerFragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_combiner, container, false);
+        View view = inflater.inflate(R.layout.fragment_combiner, container, false);
+        imageView = (ImageView) view.findViewById(R.id.fragmentImageView);
+        imageView.setOnClickListener(this);
+        if (DEBUG) Log.d(APP_TAG + activityTag, "onCreateView : View Instantiated");
+        if (cloth.getUri() != null) {
+            if (DEBUG)
+                Log.d(APP_TAG + activityTag, "onCreateView : img URI: " + cloth.getUri().toString());
+            loadBitmap(cloth.getUri(), imageView);
+
+        }
+        //else loadBitmap(c); cargar Imagen por defecto
+        return view;
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!this.getArguments().isEmpty()) {
+            cloth = this.getArguments().getParcelable(PARCELABLE_CLOTH_KEY);
+            if (DEBUG) Log.d(APP_TAG + activityTag, cloth.toString());
+        } else if (DEBUG) Log.d(APP_TAG + activityTag, "onCreate : NO BUNDLE AVAILABLE");
+    }
+
+    public void loadBitmap(String path, ImageView imageView) {
+        if (cancelPotentialWork(path, imageView)) {
+            final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+            final AsyncDrawable asyncDrawable =
+                    new AsyncDrawable(res, mPlaceHolderBitmap, task);
+            imageView.setImageDrawable(asyncDrawable);
+            task.execute(path);
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this.getActivity(), DetailsClothActivity.class);
+        intent.putExtra(ID, cloth.getId());
+        startActivity(intent);
+    }
 
 }
