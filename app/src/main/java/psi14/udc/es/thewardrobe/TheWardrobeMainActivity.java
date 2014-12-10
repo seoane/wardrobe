573c73420414053520ee9b155f90bf37c838cd04
Jesus <suso@outlook.com>
2014-12-10 22:07:35 +0100

    Navigation Drawer Created and working

--- app/src/main/java/psi14/udc/es/thewardrobe/TheWardrobeMainActivity.java
+++ app/src/main/java/psi14/udc/es/thewardrobe/TheWardrobeMainActivity.java
@@ -1,23 +1,84 @@
 package psi14.udc.es.thewardrobe;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class TheWardrobeMainActivity extends Activity {
 
     public final static String LOG_TAG = "TheWardrobeMainActivity";
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] mDrawerMenu;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_wardrobe_main);
+        mDrawerMenu = getResources().getStringArray(R.array.drawer_menu);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                android.R.drawable.ic_menu_agenda,  /* nav drawer icon to replace 'Up' caret */
+                R.string.action_add,  /* "open drawer" description */
+                R.string.action_cancel  /* "close drawer" description */
+        ) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(getResources().getString(R.string.app_name));
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(getResources().getString(R.string.app_name));
+            }
+        };
+
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        Log.d(LOG_TAG, mDrawerMenu.toString());
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mDrawerMenu);
+        mDrawerList.setAdapter(adapter);
+        // Set the list's click listener*/
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
 
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,17 +92,28 @@ public class TheWardrobeMainActivity extends Activity {
         if (id == R.id.menu_add) {
             startActivity(new Intent(this, MacClothActivity.class));
             return true;
-        } else if (id == R.id.menu_list) {
-            startActivity(new Intent(this, ListClothActivity.class));
-            return true;
-        } else if (id == R.id.menu_combiner) {
-            startActivity(new Intent(this, CombinerActivity.class));
-            return true;
-        } else if (id == R.id.menu_list_combiner) {
-            startActivity(new Intent(this, ListCombinerActivity.class));
+        }
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            switch (position) {
+                case 0:
+                    startActivity(new Intent(view.getContext(), ListClothActivity.class));
+                break;
+                case 1:
+                    startActivity(new Intent(view.getContext(), CombinerActivity.class));
+                break;
+                case 2:
+                    startActivity(new Intent(view.getContext(), ListCombinerActivity.class));
+                    break;
+            }
+        }
     }
 
 }
